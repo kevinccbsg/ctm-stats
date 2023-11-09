b8574cf5521ae412e43f36eebe10230ddeeac4c4
kevinccbsg <kevinccbsg@gmail.com>
2023-11-09 23:05:21 +0100

    refactor: simplify component

--- src/components/LifetimeStats/LifetimeStats.tsx
+++ src/components/LifetimeStats/LifetimeStats.tsx
@@ -1,33 +1,60 @@
 import { useEffect, useState } from 'react';
-import StatTable from '../../common/StatTable/StatTable';
+import { Table, Typography } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 
-interface Score {
+interface DataType {
   id: number;
   name: string;
   value: string;
 }
 
+const columns: ColumnsType<DataType> = [
+  {
+    title: '#',
+    dataIndex: 'name',
+    key: 'name',
+    render: (_val, _record, index) => <Typography.Text strong style={{ color: '#20e128'}}>{index + 1}</Typography.Text>,
+  },
+  {
+    title: 'Player',
+    dataIndex: 'name',
+    key: 'name',
+    render: (value) => <Typography.Text strong>{value}</Typography.Text>,
+  },
+  {
+    title: 'value',
+    key: 'value',
+    dataIndex: 'value',
+    align: 'right',
+  },
+];
+
 interface Props {
   title: string;
-  getStatsMethod: () => Promise<Score[]>;
+  getStatsMethod: () => Promise<DataType[]>;
 }
 
 const LifetimeStats = ({ getStatsMethod, title }: Props) => {
-  const [scores, setScore] = useState<Score[]>([]);
+  const [data, setData] = useState<DataType[]>([]);
   useEffect(() => {
     getStatsMethod()
       .then(values => {
-        setScore(values);
+        setData(values);
       })
       .catch(error => {
         console.log(error);
       });
   }, [getStatsMethod]);
   return (
-    <StatTable
-      title={title}
-      data={scores}
-    />
+    <div>
+      <Typography.Title level={4}>{title}</Typography.Title>
+      <Table
+        showHeader={false}
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+      />
+    </div>
   );
 };
 
